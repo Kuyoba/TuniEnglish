41bd35af7091469f0180e81795604c111d167f3f
Kuyoba <harounba69@gmail.com>
2026-03-11 13:42:45 +0100

    Update contact and hero styling, restore original behaviour

--- js/main.js
+++ js/main.js
@@ -42,8 +42,10 @@ var background_image_parallax_2 = function($object, multiplier){
 
 $(function(){
   // Hero Section - Background Parallax
-  background_image_parallax($(".tm-parallax"), 0.30, false);
-  background_image_parallax_2($("#contact"), 0.80);   
+  if (window.innerWidth > 767) {
+    background_image_parallax($(".tm-parallax"), 0.40, false);
+    background_image_parallax_2($("#contact"), 0.80);
+  }   
   
   // Handle window resize
   window.addEventListener('resize', function(){
